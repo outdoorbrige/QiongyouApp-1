29248f24f7b14c0f681712b66231da3195280e27
xiaohan-lh <xiaohan-lh@163.com>
2016-03-14 16:43:46 +0800

    接口定义 创建接口常量

---
+++ app/src/main/java/com/example/xiaohan_lh/qiongyouapp/bean/TabRecommend.java
@@ -0,0 +1,337 @@
+package com.example.xiaohan_lh.qiongyouapp.bean;
+
+import java.util.List;
+
+/**
+ * Created by xiaohan-lh on 16/3/14.
+ */
+public class TabRecommend {
+
+    /**
+     * status : 1
+     * info :
+     * times : 0
+     * data : {"search":"搜索目的地、帖子、特价、穷游er","slide":[{"url":"http://m.qyer.com/z/zt/2016321go0307&source=app&campaign=appshouye&category=ht1-2016321go0307?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/08/14574026028074/w800"},{"url":"http://m.qyer.com/z/zt/psjtg&source=app&campaign=appshouye&category=ht2-psjtg?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/11/14576611485458/w800"},{"url":"http://appview.qyer.com/bbs/thread-1462849-1.html?campaign=appshouye&category=ht3-1462849&source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/02/14569067792102/w800"},{"url":"http://appview.qyer.com/z/deal/68245/source=app&campaign=appshouye&category=ht4-guohangbeijing?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/11/14576877272037/w800"},{"url":"http://appview.qyer.com/bbs/thread-1362958-1.html?campaign=appshouye&category=ht5-1362958&source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/14/14579200174102/w800"},{"url":"http://appview.qyer.com/bbs/thread-1376098-1.html?campaign=appshouye&category=ht6-1376098&source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/14/14579200569624/w800"}],"subject":[{"title":"带你赏遍世界的樱花海","url":"http://appview.qyer.com/op/zt/20160311.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/appsubject/2016/03/11/14576799413868/930x380"},{"title":"京都特色店一览！","url":"http://appview.qyer.com/op/zt/20160224.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/appsubject/2016/03/11/14576807268569/450x290"},{"title":"东京特色主题餐厅大盘点","url":"http://appview.qyer.com/op/zt/20160201.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/appsubject/2016/03/04/14570592177928/450x290"}],"mguide":[],"discount":[{"id":67103,"title":"杭州直飞东京4-15天往返含税机票","price":"<em>2699<\/em>元起","priceoff":"6.7折","end_date":"2016年05月17日结束","photo":"http://pic.qyer.com/lastminute/library/2014/12/26/1419561755/600x400"},{"id":69454,"title":"【清明/五一/端午假期】杭州直飞吴哥窟4-5天往返含税机票","price":"<em>2850<\/em>元起","priceoff":"6.3折","end_date":"2016年06月16日结束","photo":"http://pic.qyer.com/lastminute/library/2015/04/02/551caa4fe5672.jpg/600x400"},{"id":69476,"title":"【端午假期】香港直飞斐济8天6晚自由行(全程五星酒店,全国联运,含3晚游轮住宿)","price":"<em>14200<\/em>元起","priceoff":"7.1折","end_date":"2016年06月28日结束","photo":"http://pic.qyer.com/lastminute/library/2015/06/29/55910c77a2992.jpg/600x400"},{"id":69429,"title":"【五一/端午假期】全国多地直飞名古屋6天往返含税机票","price":"<em>1039<\/em>元起","priceoff":"2.6折","end_date":"2016年06月26日结束","photo":"http://pic.qyer.com/lastminute/library/2015/09/07/55ed095d7ed20.jpg/600x400"}],"discount_subject":[{"url":"http://m.qyer.com/z/zt/europeart/?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homesubject/2016/03/09/14574902393627/w800"}],"seckilling":{"seckill_object":{},"seckill_switch":1},"checkin":{"status":1,"url":"http://appview.qyer.com/mileage/sign/sign"}}
+     * ra_referer :
+     */
+
+    private int status;
+    private String info;
+    private int times;
+    /**
+     * search : 搜索目的地、帖子、特价、穷游er
+     * slide : [{"url":"http://m.qyer.com/z/zt/2016321go0307&source=app&campaign=appshouye&category=ht1-2016321go0307?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/08/14574026028074/w800"},{"url":"http://m.qyer.com/z/zt/psjtg&source=app&campaign=appshouye&category=ht2-psjtg?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/11/14576611485458/w800"},{"url":"http://appview.qyer.com/bbs/thread-1462849-1.html?campaign=appshouye&category=ht3-1462849&source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/02/14569067792102/w800"},{"url":"http://appview.qyer.com/z/deal/68245/source=app&campaign=appshouye&category=ht4-guohangbeijing?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/11/14576877272037/w800"},{"url":"http://appview.qyer.com/bbs/thread-1362958-1.html?campaign=appshouye&category=ht5-1362958&source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/14/14579200174102/w800"},{"url":"http://appview.qyer.com/bbs/thread-1376098-1.html?campaign=appshouye&category=ht6-1376098&source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homebanner/2016/03/14/14579200569624/w800"}]
+     * subject : [{"title":"带你赏遍世界的樱花海","url":"http://appview.qyer.com/op/zt/20160311.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/appsubject/2016/03/11/14576799413868/930x380"},{"title":"京都特色店一览！","url":"http://appview.qyer.com/op/zt/20160224.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/appsubject/2016/03/11/14576807268569/450x290"},{"title":"东京特色主题餐厅大盘点","url":"http://appview.qyer.com/op/zt/20160201.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/appsubject/2016/03/04/14570592177928/450x290"}]
+     * mguide : []
+     * discount : [{"id":67103,"title":"杭州直飞东京4-15天往返含税机票","price":"<em>2699<\/em>元起","priceoff":"6.7折","end_date":"2016年05月17日结束","photo":"http://pic.qyer.com/lastminute/library/2014/12/26/1419561755/600x400"},{"id":69454,"title":"【清明/五一/端午假期】杭州直飞吴哥窟4-5天往返含税机票","price":"<em>2850<\/em>元起","priceoff":"6.3折","end_date":"2016年06月16日结束","photo":"http://pic.qyer.com/lastminute/library/2015/04/02/551caa4fe5672.jpg/600x400"},{"id":69476,"title":"【端午假期】香港直飞斐济8天6晚自由行(全程五星酒店,全国联运,含3晚游轮住宿)","price":"<em>14200<\/em>元起","priceoff":"7.1折","end_date":"2016年06月28日结束","photo":"http://pic.qyer.com/lastminute/library/2015/06/29/55910c77a2992.jpg/600x400"},{"id":69429,"title":"【五一/端午假期】全国多地直飞名古屋6天往返含税机票","price":"<em>1039<\/em>元起","priceoff":"2.6折","end_date":"2016年06月26日结束","photo":"http://pic.qyer.com/lastminute/library/2015/09/07/55ed095d7ed20.jpg/600x400"}]
+     * discount_subject : [{"url":"http://m.qyer.com/z/zt/europeart/?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5","photo":"http://pic.qyer.com/public/mobileapp/homesubject/2016/03/09/14574902393627/w800"}]
+     * seckilling : {"seckill_object":{},"seckill_switch":1}
+     * checkin : {"status":1,"url":"http://appview.qyer.com/mileage/sign/sign"}
+     */
+
+    private DataEntity data;
+    private String ra_referer;
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setTimes(int times) {
+        this.times = times;
+    }
+
+    public void setData(DataEntity data) {
+        this.data = data;
+    }
+
+    public void setRa_referer(String ra_referer) {
+        this.ra_referer = ra_referer;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public int getTimes() {
+        return times;
+    }
+
+    public DataEntity getData() {
+        return data;
+    }
+
+    public String getRa_referer() {
+        return ra_referer;
+    }
+
+    public static class DataEntity {
+        private String search;
+        /**
+         * seckill_object : {}
+         * seckill_switch : 1
+         */
+
+        private SeckillingEntity seckilling;
+        /**
+         * status : 1
+         * url : http://appview.qyer.com/mileage/sign/sign
+         */
+
+        private CheckinEntity checkin;
+        /**
+         * url : http://m.qyer.com/z/zt/2016321go0307&source=app&campaign=appshouye&category=ht1-2016321go0307?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5
+         * photo : http://pic.qyer.com/public/mobileapp/homebanner/2016/03/08/14574026028074/w800
+         */
+
+        private List<SlideEntity> slide;
+        /**
+         * title : 带你赏遍世界的樱花海
+         * url : http://appview.qyer.com/op/zt/20160311.html?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5
+         * photo : http://pic.qyer.com/public/mobileapp/appsubject/2016/03/11/14576799413868/930x380
+         */
+
+        private List<SubjectEntity> subject;
+        /**
+         * id : 67103
+         * title : 杭州直飞东京4-15天往返含税机票
+         * price : <em>2699</em>元起
+         * priceoff : 6.7折
+         * end_date : 2016年05月17日结束
+         * photo : http://pic.qyer.com/lastminute/library/2014/12/26/1419561755/600x400
+         */
+
+        private List<DiscountEntity> discount;
+        /**
+         * url : http://m.qyer.com/z/zt/europeart/?source=app&client_id=qyer_android&track_user_id=&track_deviceid=863784027041925&track_app_version=6.8.5
+         * photo : http://pic.qyer.com/public/mobileapp/homesubject/2016/03/09/14574902393627/w800
+         */
+
+        private List<DiscountSubjectEntity> discount_subject;
+
+        public void setSearch(String search) {
+            this.search = search;
+        }
+
+        public void setSeckilling(SeckillingEntity seckilling) {
+            this.seckilling = seckilling;
+        }
+
+        public void setCheckin(CheckinEntity checkin) {
+            this.checkin = checkin;
+        }
+
+        public void setSlide(List<SlideEntity> slide) {
+            this.slide = slide;
+        }
+
+        public void setSubject(List<SubjectEntity> subject) {
+            this.subject = subject;
+        }
+
+        public void setDiscount(List<DiscountEntity> discount) {
+            this.discount = discount;
+        }
+
+        public void setDiscount_subject(List<DiscountSubjectEntity> discount_subject) {
+            this.discount_subject = discount_subject;
+        }
+
+        public String getSearch() {
+            return search;
+        }
+
+        public SeckillingEntity getSeckilling() {
+            return seckilling;
+        }
+
+        public CheckinEntity getCheckin() {
+            return checkin;
+        }
+
+        public List<SlideEntity> getSlide() {
+            return slide;
+        }
+
+        public List<SubjectEntity> getSubject() {
+            return subject;
+        }
+
+        public List<DiscountEntity> getDiscount() {
+            return discount;
+        }
+
+        public List<DiscountSubjectEntity> getDiscount_subject() {
+            return discount_subject;
+        }
+
+        public static class SeckillingEntity {
+            private int seckill_switch;
+
+            public void setSeckill_switch(int seckill_switch) {
+                this.seckill_switch = seckill_switch;
+            }
+
+            public int getSeckill_switch() {
+                return seckill_switch;
+            }
+        }
+
+        public static class CheckinEntity {
+            private int status;
+            private String url;
+
+            public void setStatus(int status) {
+                this.status = status;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public int getStatus() {
+                return status;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+        }
+
+        public static class SlideEntity {
+            private String url;
+            private String photo;
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public void setPhoto(String photo) {
+                this.photo = photo;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public String getPhoto() {
+                return photo;
+            }
+        }
+
+        public static class SubjectEntity {
+            private String title;
+            private String url;
+            private String photo;
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public void setPhoto(String photo) {
+                this.photo = photo;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public String getPhoto() {
+                return photo;
+            }
+        }
+
+        public static class DiscountEntity {
+            private int id;
+            private String title;
+            private String price;
+            private String priceoff;
+            private String end_date;
+            private String photo;
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public void setPrice(String price) {
+                this.price = price;
+            }
+
+            public void setPriceoff(String priceoff) {
+                this.priceoff = priceoff;
+            }
+
+            public void setEnd_date(String end_date) {
+                this.end_date = end_date;
+            }
+
+            public void setPhoto(String photo) {
+                this.photo = photo;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public String getPrice() {
+                return price;
+            }
+
+            public String getPriceoff() {
+                return priceoff;
+            }
+
+            public String getEnd_date() {
+                return end_date;
+            }
+
+            public String getPhoto() {
+                return photo;
+            }
+        }
+
+        public static class DiscountSubjectEntity {
+            private String url;
+            private String photo;
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public void setPhoto(String photo) {
+                this.photo = photo;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public String getPhoto() {
+                return photo;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/xiaohan_lh/qiongyouapp/fragment/RecommendFragment.java
+++ app/src/main/java/com/example/xiaohan_lh/qiongyouapp/fragment/RecommendFragment.java
@@ -16,14 +16,12 @@ public class RecommendFragment extends Fragment {
 
 
     public RecommendFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recommend, container, false);
     }
 
