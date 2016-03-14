236757974209db2253a5086c32000b5d58e994bf
xiaohan-lh <xiaohan-lh@163.com>
2016-03-14 15:32:03 +0800

    创建文件实现mvp

---
+++ app/src/main/java/com/example/xiaohan_lh/qiongyouapp/App.java
@@ -0,0 +1,14 @@
+package com.example.xiaohan_lh.qiongyouapp;
+
+import android.app.Application;
+
+/**
+ * Created by xiaohan-lh on 16/3/14.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/com/example/xiaohan_lh/qiongyouapp/ui/MainActivity.java
+++ app/src/main/java/com/example/xiaohan_lh/qiongyouapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.xiaohan_lh.qiongyouapp;
+package com.example.xiaohan_lh.qiongyouapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.xiaohan_lh.qiongyouapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
