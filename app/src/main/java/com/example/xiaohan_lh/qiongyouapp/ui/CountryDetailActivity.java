88a3c4eedec480f55c45ad1552164792ae43fa4c
CHENGHAN1115 <1162759636@qq.com>
2016-03-15 15:49:24 +0800

    destination ok

--- app/src/main/java/com/example/xiaohan_lh/qiongyouapp/ui/CountryDetailActivity.java
+++ app/src/main/java/com/example/xiaohan_lh/qiongyouapp/ui/CountryDetailActivity.java
@@ -16,6 +16,6 @@ public class CountryDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_detail);
-//
+
     }
 }
